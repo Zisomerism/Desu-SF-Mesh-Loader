db34acb0bad5435ba536a901222544af332e4ad5
Zisomerism <h3lplessalpaca@gmail.com>
2024-06-15 03:05:07 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 local OBJFileURL = ""
-local meshScale = 10
+local meshScale = 50
 local objectNames = {}
 local textureURLs = {}
 local textureFlags = {}
@@ -41,20 +41,20 @@ local function setHoloMesh(holo, mesh_, texture)
     holo:setRenderBounds(Vector(-1000),Vector(1000)) --adjust if you're loading something huge or something with massively offset meshes (which you probably shouldn't be doing), otherwise this should be fine.
 end
 
-http.get(OBJFileURL,function(objdata)  
-    for k,v in ipairs(objectNames) do
-        local loadMesh = coroutine.wrap(function() meshLoadedFromObj = mesh.createFromObj(objdata, true, true)[objectNames[k]] return true end) 
-        hook.add("think",objectNames[k],function()
-            while math.max(quotaAverage(), quotaUsed()) < quotaMax() /10 do
-                if loadMesh() then
-                    setHoloMesh(createHolo(), meshLoadedFromObj, createTexture(textureURLs[k],textureFlags[k]))
-                    print("Finished loading:", objectNames[k], "with texture:", textureURLs[k])
-                    hook.remove("think",objectNames[k])
-                    return
+local function createMeshFromOBJ()
+    http.get(OBJFileURL,function(objdata)  
+        for k,v in ipairs(objectNames) do
+            local loadMesh = coroutine.wrap(function() meshLoadedFromObj = mesh.createFromObj(objdata, true, true)[objectNames[k]] return true end) 
+            hook.add("think",objectNames[k],function()
+                while math.max(quotaAverage(), quotaUsed()) < quotaMax() /10 do
+                    if loadMesh() then
+                        setHoloMesh(createHolo(), meshLoadedFromObj, createTexture(textureURLs[k],textureFlags[k]))
+                        print("Finished loading:", objectNames[k], "with texture:", textureURLs[k])
+                        hook.remove("think",objectNames[k])
+                        return
+                    end
                 end
-            end
-        end)
-    end
-end)   
-
-return 1
+            end)
+        end
+    end)  
+end 
